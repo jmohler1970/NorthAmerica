ce8aac4ba985b5ff1791a39efcadb0457bbf33aa
James Mohler <james.mohler@me.com>
2018-10-23 18:39:58 -0700

    Now with drops

---
+++ drop_db.sql
@@ -0,0 +1,7 @@
+EXEC msdb.dbo.sp_delete_database_backuphistory @database_name = N'NorthAmerica'
+GO
+USE [master]
+GO
+/****** Object:  Database [TSqlToolbox]    Script Date: 10/23/18 6:37:28 PM ******/
+DROP DATABASE [NorthAmerica]
+GO
